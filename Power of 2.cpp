6ba87f29c6cc440f3d197a4bd4617f5d80d633c5
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2023-09-18 23:36:27 +0530

    Create Power of 2.cpp

---
+++ Power of 2.cpp
@@ -0,0 +1,14 @@
+// https://practice.geeksforgeeks.org/problems/power-of-2-1587115620/1
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here   
+        if ( n==0) return false;
+        
+        return  (n & (n-1)) == 0; 
+        
+    }
+};
