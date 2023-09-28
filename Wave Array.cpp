9bda1160baa06faf47be603c3faedb376262c5b9
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2023-09-28 22:52:57 +0530

    Create Wave Array.cpp

---
+++ Wave Array.cpp
@@ -0,0 +1,22 @@
+// https://practice.geeksforgeeks.org/problems/wave-array-1587115621/1
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(int n, vector<int>& arr){
+        
+        if (n % 2 != 0){
+            for(int i = 1; i < n; i+=2 ){
+                swap(arr[i],arr[i-1]);
+            }
+        } 
+        else{
+            for(int i = 1; i<n; i+=2){
+                swap(arr[i],arr[i-1]);
+            }
+        }
+        
+    }
+};
