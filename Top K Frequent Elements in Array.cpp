16e401e5efae8be0ba4d0275f935ac7de6d1dc39
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2023-11-05 22:26:45 +0530

    Create Top K Frequent Elements in Array.cpp

---
+++ Top K Frequent Elements in Array.cpp
@@ -0,0 +1,23 @@
+// https://www.geeksforgeeks.org/problems/top-k-frequent-elements-in-array/1
+
+public:
+    vector<int> topK(vector<int>& nums, int k) {
+        vector<int> v;
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        priority_queue<pair<int,int>>q;
+        for(auto x:m)
+        {
+            q.push({x.second,x.first});
+        }
+        while(k!=0)
+        {
+            v.push_back(q.top().second);
+            q.pop();
+            k--;
+        }
+        return v;
+    }
