23c98ecd8ff2132029a33b19bc35be946a4fc63c
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2023-11-15 20:37:50 +0530

    Create Better String.cpp

---
+++ Better String.cpp
@@ -0,0 +1,19 @@
+// https://www.geeksforgeeks.org/problems/better-string/1
+
+class Solution {
+  public:
+  
+    int disSubseq(string &s){
+        long endsWith[26] = {};
+        for (char c : s)
+            endsWith[c - 'a'] = accumulate(begin(endsWith), end(endsWith), 1L);
+        return accumulate(begin(endsWith), end(endsWith), 0L) + 1;
+    }
+    
+    string betterString(string str1, string str2) {
+        int l1 = disSubseq(str1);
+        int l2 = disSubseq(str2);
+        
+        return (l1 > l2 ? str1 : (l1 == l2 ? str1 : str2));
+    }
+};
