4ffca4b7abf288025def7989b31573af1c7022d5
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2023-11-17 23:16:25 +0530

    Create Binary Tree to CDLL.cpp

---
+++ Binary Tree to CDLL.cpp
@@ -0,0 +1,33 @@
+// https://www.geeksforgeeks.org/problems/binary-tree-to-cdll/1
+
+class Solution
+{
+  public:
+    //Function to convert binary tree into circular doubly linked list.
+    void solve(Node* root , Node* &head){
+        
+        if(root == NULL){
+            return;
+        }
+        
+        solve(root->right,head);
+        root->right = head;
+        if(head != NULL){
+            head->left = root;
+        }
+        head = root;
+        solve(root->left,head);
+        
+    }
+    Node *bTreeToCList(Node *root)
+    {
+    Node* head = NULL;
+        solve(root,head);
+        Node* tail = head;
+        while(tail->right != NULL){
+            tail = tail->right;
+        }
+        tail->right = head;
+        head->left = tail;
+        return head;
+    }
