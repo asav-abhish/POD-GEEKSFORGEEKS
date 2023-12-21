2cb46abd4a1c887e26f7802a0d17848ff1304524
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2023-12-21 22:22:05 +0530

    Create Candy.cpp

---
+++ Candy.cpp
@@ -0,0 +1,25 @@
+// https://www.geeksforgeeks.org/problems/candy/1
+
+class Solution {
+  public:
+    int minCandy(int N, vector<int> &ratings) {
+        vector<int> v(N,1);
+        for(int i=1;i<N;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            v[i]=v[i-1]+1;
+        }
+        
+        for(int i=N-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            v[i]=max(v[i],v[i+1]+1);
+        }
+        
+        int sum=0;
+        for(int i=0;i<N;i++)
+        sum+=v[i];
+        
+        return sum;
+    }
+};
