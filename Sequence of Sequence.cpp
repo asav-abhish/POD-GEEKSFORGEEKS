e6e8158f8fc6f2760e2f27e568e459f92eec017e
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-01-16 23:39:22 +0530

    Create Sequence of Sequence.cpp

---
+++ Sequence of Sequence.cpp
@@ -0,0 +1,23 @@
+// https://www.geeksforgeeks.org/problems/sequence-of-sequence1155/1
+
+class Solution{
+public:
+    int solve(int i,int j,int m,int n){
+        
+        if( i > m) return 0;
+        if( j == n ) return 1;
+        int ans = 0;
+        for(int k = 2*i;k<=m;k++){
+            ans += solve(k,j+1,m,n);
+        }
+        return ans;
+    }
+    
+    int numberSequence(int m, int n){
+        int ans = 0;
+        for(int i = 1;i<=m;i++){
+            ans += solve(i,1,m,n);
+        }
+        return ans;
+    }
+};
