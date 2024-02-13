a365649819dfc6a80b0a08d44972d5f9d8783328
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-02-13 21:17:10 +0530

    Create Clone an Undirected Graph.cpp

---
+++ Clone an Undirected Graph.cpp
@@ -0,0 +1,24 @@
+// https://www.geeksforgeeks.org/problems/clone-graph/1
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) {
+            return NULL;
+        }
+        // If the node is not present in the map, create a copy of it
+        if (copies.find(node) == copies.end()) {
+            // Create a new node with the same value as the original node
+            copies[node] = new Node(node -> val, {});
+            // Copy the neighbors of the original node recursively
+            for (Node* neighbor : node -> neighbors) {
+                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
+            }
+        }
+        // Return the copy of the node
+        return copies[node];
+    }
+private:
+    // Map to store the copy of the nodes
+    unordered_map<Node*, Node*> copies;
+};
