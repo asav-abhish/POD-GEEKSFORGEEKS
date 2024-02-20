5ff8b29f2342a334695d1cf11d2d063be336967b
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-02-21 00:07:57 +0530

    Create Boolean Parenthesization.cpp

---
+++ Boolean Parenthesization.cpp
@@ -0,0 +1,44 @@
+// https://www.geeksforgeeks.org/problems/boolean-parenthesization5610/1
+
+class Solution{
+public:
+    int mod = 1003;
+    int solve(int i, int j, int isTrue, string& S,vector<vector<vector<long long>>>& dp) {
+        if(i>j) return 0;
+        if(i==j) {
+            if(isTrue) {
+                return S[i]=='T';
+            }
+            else {
+                return S[i]=='F';
+            }
+        }
+        if(dp[i][j][isTrue]!=-1) return dp[i][j][isTrue];
+        int ways=0;
+        for(long long ind=i+1;ind<=j-1;ind=ind+2) {
+            long long lT = solve(i,ind-1,1,S,dp);
+            long long lF = solve(i,ind-1,0,S,dp);
+            long long rT = solve(ind+1,j,1,S,dp);
+            long long rF = solve(ind+1,j,0,S,dp);
+            
+            if(S[ind]== '&') {
+                if(isTrue) ways = (ways + (lT*rT)%mod)%mod;
+                else ways = (ways + (lT*rF)%mod + (lF*rT)%mod + (lF*rF)%mod)%mod;
+            }
+            else if(S[ind]== '|') {
+                if(isTrue) ways = (ways + (lT*rT)%mod + (lT*rF)%mod + (lF*rT)%mod)%mod;
+                else ways = (ways + (lF*rF)%mod)%mod;
+            }
+            else {
+                if(isTrue) ways = (ways + (lT*rF)%mod + (lF*rT)%mod) % mod;
+                else  ways = (ways + (lT*rT)%mod + (lF*rF)%mod);
+            }
+        }
+        return dp[i][j][isTrue] = ways;
+    }
+    int countWays(int n, string s){
+        // code here
+        vector<vector<vector<long long>>>dp(n,vector<vector<long long>>(n,vector<long long>(2,-1)));
+        return solve(0,n-1,1,s,dp);
+    }
+};
