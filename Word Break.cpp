c0808270d91f36a619d23dc8829d64873a68d373
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-02-20 21:25:18 +0530

    Create Word Break.cpp

---
+++ Word Break.cpp
@@ -0,0 +1,25 @@
+// https://www.geeksforgeeks.org/problems/word-break1352/1
+
+class Solution
+{
+public:
+    int help(int i,string s,set<string>&dis){
+        if(i==s.size())return 1;
+        string temp;
+        for(int j=i; j<s.size(); j++){
+            temp+=s[j];
+            if(dis.find(temp)!=dis.end()){
+                if(help(j+1,s,dis))return 1;
+            }
+        }
+        return 0;
+    }
+    int wordBreak(int n, string s, vector<string> &dictionary) {
+        //code here
+        set<string>st;
+        for(auto a:dictionary){
+            st.insert(a);
+        }
+        return help(0,s,st);
+    }
+};
