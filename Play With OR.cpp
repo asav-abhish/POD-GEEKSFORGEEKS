f1179c9ddfc6faacb16b2e980b2ce69ac76eb685
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-02-27 21:27:17 +0530

    Create Play With OR.cpp

---
+++ Play With OR.cpp
@@ -0,0 +1,9 @@
+// https://www.geeksforgeeks.org/problems/play-with-or5515/1
+
+int* game_with_number(int arr[], int n)
+{
+    for(int i=0; i<n-1; i++) {
+        arr[i] = arr[i] | arr[i+1];
+    }
+    return arr;
+}
