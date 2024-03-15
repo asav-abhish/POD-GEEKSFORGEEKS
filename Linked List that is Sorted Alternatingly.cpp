78c6080c9c7ab98ecef46f862acba23c40dac181
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-03-15 22:33:46 +0530

    Create Linked List that is Sorted Alternatingly.cpp

---
+++ Linked List that is Sorted Alternatingly.cpp
@@ -0,0 +1,27 @@
+// https://www.geeksforgeeks.org/problems/linked-list-that-is-sorted-alternatingly/1
+
+class Solution
+{
+    public:
+    // your task is to complete this function
+    void sort(Node **head)
+    {
+         // Code here
+          if((*head)==NULL || (*head)->next==NULL) return ;
+         Node *temp=*head;
+         vector<int>v;
+         while(temp){
+             auto val=temp->data;
+             v.push_back(val);
+             temp=temp->next;
+         }
+         
+        std:: sort(v.begin(),v.end());
+         
+         temp=*head;
+         
+        for(int i=0;i<v.size();i++){
+            temp->data=v[i];
+            temp=temp->next;
+        }
+    }
