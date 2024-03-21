6274eb7bb14e73e3ca8593409f09512ae580f1a7
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-03-21 21:05:00 +0530

    Create ZigZag Tree Traversal.cpp

---
+++ ZigZag Tree Traversal.cpp
@@ -0,0 +1,27 @@
+// https://www.geeksforgeeks.org/problems/zigzag-tree-traversal/1
+
+ bool direction = true;
+        queue<Node*>q;
+        vector<int>ans;
+        if(!root) return ans;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size = q.size();
+            vector<int>temp;
+            for(int i = 0;i<size;i++)
+            {
+                root = q.front();
+                q.pop();
+                temp.push_back(root->data);
+                if(root->left) q.push(root->left);
+                if(root->right) q.push(root->right);
+            }
+            if(!direction)
+            {
+                reverse(temp.begin(),temp.end());
+            }
+            direction = !direction;
+            ans.insert(ans.end(), temp.begin(), temp.end());
+        }
+        return ans;
