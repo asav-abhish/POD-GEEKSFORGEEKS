a5d98e947caf7472d5f7e184c33cf40dbb6b151b
Abhishek Kumar <87856621+asav-abhish@users.noreply.github.com>
2024-05-09 23:35:02 +0530

    Create Divisor Game.cpp

---
+++ Divisor Game.cpp
@@ -0,0 +1,12 @@
+// https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1
+
+public:
+    bool divisorGame(int n) {
+        // code here
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
